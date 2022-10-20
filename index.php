8f5b7e73b1c04c10da85aa8e78cf8530484a2006
ancyanees <ancyanees786@gmail.com>
2022-10-20 11:54:35 -0700

    commit googlesite

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>My Own Site</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="fa/css/all.min.css">
+	</head>
+	<body>	
+	<!-- section1-->
+        <section class="bg-img vh-100">
+            <div class="bg-back vh-100">
+                <nav class="navbar navbar-expand-lg navbar-light bg-col">
+                    <div class="container-fluid">
+                      <a class="navbar-brand" href="#"><h2><span class="text-primary">G</span><span class="text-danger">o</span><span class="text-warning">o</span><span class="text-primary">g</span><span class="text-success">l</span><span class="text-danger">e</span> Drive</h2></a>
+                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                          </button>
+
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Meet Drive <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Using Drive</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#">Download</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#">For Business</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#">Help</a>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+
+                          <button class="btn btn-primary py-md-4 px-md-4 my-2 my-sm-0" type="submit">Go To Google Drive</button>
+                        </form>
+                      </div>
+                    </div>
+                </nav>
+                
+            <!--div starts-->    
+                <div class="text-light text-center py-md-3 style-cls">
+                    <img src="img/7.png" class="img-fluid img-stl" width="5%;">
+                    <p>A safe place for all your files</p>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 text-center main-col">
+                            <div class="subcol col-st">
+                                <h3 class="py-md-2">Personal</h3>
+                                <p class="p-md-4">Store, share, and access your files from any device. Your first 15 GB of storage are free.</p>
+                                <button class="btn btn-primary py-md-3 px-md-5  my-2 my-sm-0" type="submit">Go To Google Drive</button>
+                            </div>
+                        </div>
+						<div class="col-md-6 text-center main-col1">
+                            <div class="subcol col-st1">
+                                <h3 class="py-md-2">Business</h3>
+                                <p class="p-md-3">With Drive Enterprise, businesses only pay for the storage employees use. It comes with Google Docs, Sheets, and Slides — and works seamlessly with Microsoft Office.</p>
+                                <button class="btn btn-primary py-md-3 px-md-5  my-2 my-sm-0" type="submit">Learn More</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+			<!--ends div-->
+            </div>
+        </section>  
+    <!-- ends section-->
+	<!--starts section2-->
+		<section>
+			<div class="container-fluid">
+				<div class="row py-md-5 mt-md-5">
+					<div class="col-md-2">
+					</div>
+					<div class="col-md-5 img-class">
+						<img src="img/9.jpg" class="img-fluid">
+					</div>
+					<div class="col-md-5 py-md-5 mt-md-2 col-class">
+						<p class="p-style">Store any file<p>
+						<p class="p-style1 text-dark">Keep photos, stories, designs, drawings,</br> recordings, videos, and more. Your first 15 GB</br> of storage are free with a Google Account.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	<!--ends section2-->
+	<!-- starts section3-->
+		<section>
+			<div class="py-md-5 class-img">
+				<img src="img/8.jpg" class="img-fluid"  width="100%;">
+			</div>
+		</section>
+	<!-- ends section3-->
+	
+	<!--starts section4-->
+		<section>
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-md-5 py-md-5 mt-md-5 text-style">
+						<p class="p-style2">See your </br>stuff anywhere</p>
+						<p class=" p1-style text-dark">Your files in Drive can be reached from any</br> smartphone, tablet, or computer. So wherever</br> you go, your files follow.</p>
+					</div>
+					<div class="col-md-7 img-style">
+						<img src="img/1.jpg" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</section>
+	<!--ends section4-->
+	<!-- starts section5-->
+		<section>
+			<div class="py-md-5 image-stl">
+				<img src="img/3.jpg" class="img-fluid"  width="100%;">
+			</div>
+		</section>
+	<!-- ends section5-->
+    <!-- starts section6-->
+		<section>
+			<div class="py-md-5 text-center image1">
+				<img src="img/10.jpg" class="img-fluid">
+			</div>
+			<div class="text-center text-dark text">
+				<p class="p-style">Share files and folders</p>
+				<p class="p-style1">You can quickly invite others to view, download, and collaborate on all the</br> files you want–no email attachment needed.</p>
+			</div>
+		</section>
+	<!-- ends section6-->
+	<!-- starts section7-->
+		<section>
+			<div class="py-md-5 image-stl">
+				<img src="img/11.jpg" class="img-fluid"  width="100%;">
+			</div>
+		</section>
+	<!-- ends section7-->
+	<!--starts section8-->
+		<section>
+			<div class="text-dark text-center py-md-5 styles">
+                <img src="img/7.png" class="img-fluid img-stl" width="7%;">
+                <p class="p-style">Get started with Drive for free</p>
+				<a href="#">Need more than 15 GB? Upgrade to Google One </a>
+            </div>
+		</section>
+	<!--ends section8-->
+	
+	<!-- Footer -->
+    
+
+	<footer>
+		<div class="container foot-cls">
+			<div class="py-md-3">
+				Follow:
+				<span class="follow">
+				  <a href="#"><img src="img/twitter.png" ></a>
+				  <a href="#"><img src="img/blogger.jpg"></a>
+				  
+				</span>
+			  </div>
+			  <div class="div-style py-md-1"></div>
+			
+
+			<div class="row py-md-5 foot">
+			  <div class="footer-unit">
+				<h5 class="text-center">About Drive</h5>
+				<ul>
+				  <li><a href="#">Meet Google Drive</a></li>
+				  <li><a href="#">Using Drive</a></li>
+				  <li><a href="#">Get Started with Drive</a></li>
+				</ul>
+			  </div>
+			  <div class="footer-unit ml-3">
+				<h5 class="text-center">Storage plans and pricing</h5>
+				<ul>
+				  <li><a href="#">Get more storage</a></li>
+				  <li><a href="#">Pricing FAQs</a></li>
+				  <li><a href="#">Free up space</a></li>
+				</ul>
+			  </div>
+			  <div class="footer-unit ml-md-5">
+				<h5 class="text-center">Downloads</h5>
+				<ul>
+				  <li><a href="#">Drive for Mac/PC</a></li>
+				  <li><a href="#">Drive for Android</a></li>
+				  <li><a href="#">Drive for iOS</a></li>
+				</ul>
+			  </div>
+			  <div class="footer-unit ml-md-3">
+				<h5 class="text-center">Support</h5>
+				<ul>
+				  <li><a href="#">Help Center for Drive</a></li>
+				  <li><a href="#">Google Drive Help Forum</a></li>
+				  <li><a href="#">Contact Us</a></li>
+				</ul>
+			  </div>
+			  <div class="footer-unit">
+				<h5 class="text-center">Related Solutions</h5>
+				<ul>
+				  <li><a href="#">Google Docs</a></li>
+				  <li><a href="#">Google Sheets</a></li>
+				  <li><a href="#">Google Slides</a></li>
+				  <li><a href="#">Google Forms</a></li>
+				  <li><a href="#">Google Drive for Work</a></li>
+				  <li><a href="#">Google Cloud Platform</a></li>
+				  <li><a href="#">Google Apps for Education</a></li>
+				  <li><a href="#">Google Apps for Government</a></li>
+				  <li><a href="#">Google Business Solutions</a></li>
+				</ul>
+			  </div>
+			</div>
+
+		</div>
+		<div class="ul-style p-md-1">
+			<ul class="d-flex ml-md-3">
+				<li><a href="#">Google</a></li>
+				<li><a href="#" class="p-md-2">About Google</a></li>
+				<li><a href="#" class="p-md-2">Privacy</a></li>
+				<li><a href="#" class="p-md-2">Terms</a></li>
+			</ul>
+			<div class="select-style text-right px-md-5">
+				<select class="selct">
+					<option value="English(United States)">English(United States)</option>
+				</select>
+			</div>
+		</div>
+		
+			
+		
+	</footer>
+	
+	
+		<script src="js/jquery.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
